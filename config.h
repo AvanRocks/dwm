0f17504c64b464727158f7c7873aa5e917305ac8
AvanRocks <64660241+AvanRocks@users.noreply.github.com>
2020-08-02 15:15:35 -0400

    aligned spacing in code

--- config.h
+++ config.h
@@ -70,7 +70,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_u, zoom,           {0} },
+	{ MODKEY,                       XK_u,	   zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_v,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },

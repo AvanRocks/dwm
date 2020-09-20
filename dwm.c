5e6a6f4b98e4c773c1963a4f8f97c60de24da20c
AvanRocks <64660241+AvanRocks@users.noreply.github.com>
2020-09-19 20:35:36 -0400

    fixed bug where floating windows would be teleported across monitors if moved to the right half of a monitor (the bug was due to an incorrect manual merge of the statuscolors patch which was intended for an older version of dwm. The bug was in drawbar() in dwm.c)

--- dwm.c
+++ dwm.c
@@ -729,7 +729,6 @@ drawbar(Monitor *m)
 	if (m == selmon) { /* status is only drawn on selected monitor */
 		drw_setscheme(drw, scheme[SchemeNorm]);
 		tw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
-		sw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
 		while (1) {
 			if ((unsigned int)*ts > LENGTH(colors)) { ts++; continue ; }
 			ctmp = *ts;
